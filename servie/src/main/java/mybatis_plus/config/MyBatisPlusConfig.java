a3ed9ac4f0460bb50c6a258826157fd7712e1063
lzt <2737299165@qq.com>
2020-06-12 22:52:11 +0800

    Redis1

---
+++ servie/src/main/java/mybatis_plus/config/MyBatisPlusConfig.java
@@ -0,0 +1,45 @@
+package mybatis_plus.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author lzt
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.mybatis_plus.mapper")
+public class MyBatisPlusConfig {
+    /**
+     * 乐观锁
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 分页
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 逻辑删除
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+}
